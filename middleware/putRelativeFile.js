9b5eaabbe9b983b1e81aeb840286e6495d073134
Michael Bowen <mikee.bowen@gmail.com>
2021-05-20 09:27:14 -0700

    Pass: PutRelativeFile.FileNameReturnedIsCorrectlyEncoded

--- middleware/putRelativeFile.js
+++ middleware/putRelativeFile.js
@@ -1,6 +1,7 @@
 'use strict'
 const { parse, join } = require('path')
 const { readdir } = require('fs/promises')
+const { decode } = require('utf7')
 const { fileInfo, updateFile } = require('../utils')
 const wopiStorageFolder = process.env.WOPI_STORAGE.split(',')
 
@@ -9,6 +10,7 @@ module.exports = async (req, res, next) => {
   const isSuggested = req.header('X-WOPI-SuggestedTarget')
   const overwriteHeader = req.header('X-WOPI-OverwriteRelativeTarget')
   const overwrite = (overwriteHeader && overwriteHeader.toLowerCase() === 'true') || false
+
   if (!!isRelative === !!isSuggested) {
     return res.sendStatus(501)
   }
@@ -16,18 +18,18 @@ module.exports = async (req, res, next) => {
   if (isSuggested) {
     try {
       const fileName = isSuggested.startsWith('.') ? fileInfo.info.BaseFileName + isSuggested : isSuggested
-      let newFileName = fileName
+      const decodedFileName = decode(fileName)
+      let newFileName = decodedFileName
       const folderPath = join(parse(process.cwd()).root, ...wopiStorageFolder)
 
       const files = await readdir(folderPath)
       let count = 1
       while (files.includes(newFileName)) {
-        newFileName = `v${count}.${fileName}`
+        newFileName = `v${count}.${decodedFileName}`
         count++
       }
       const filePath = join(folderPath, newFileName)
       await updateFile(filePath, req.rawBody, true)
-      // res.status(200)
       const myUrl = new URL(`http://localhost:3000/wopi/files/${newFileName}`)
       myUrl.searchParams.append('access_token', 'myVerySecretToken')
       const data = JSON.stringify({
@@ -42,24 +44,25 @@ module.exports = async (req, res, next) => {
   } else {
     try {
       const fileName = isRelative.startsWith('.') ? fileInfo.info.BaseFileName + isRelative : isRelative
+      const newFileName = decode(fileName)
       const folderPath = join(parse(process.cwd()).root, ...wopiStorageFolder)
-      const filePath = join(folderPath, fileName)
-      const exists = (await readdir(folderPath)).includes(fileName)
-      const isLocked = Object.hasOwnProperty.call(fileInfo.lock, fileName)
+      const filePath = join(folderPath, newFileName)
+      const exists = (await readdir(folderPath)).includes(newFileName)
+      const isLocked = Object.hasOwnProperty.call(fileInfo.lock, newFileName)
 
       if (overwrite && !exists) {
         await updateFile(filePath, req.rawBody, false)
         res.status(200)
       } else {
         if (isLocked) {
-          res.setHeader('X-WOPI-Lock', fileInfo.lock[fileName] || '')
+          res.setHeader('X-WOPI-Lock', fileInfo.lock[newFileName] || '')
         }
         res.status(409)
       }
-      const myUrl = new URL(`http://localhost:3000/wopi/files/${fileName}`)
+      const myUrl = new URL(`http://localhost:3000/wopi/files/${newFileName}`)
       myUrl.searchParams.append('access_token', 'myVerySecretToken')
       return res.json({
-        Name: fileName,
+        Name: newFileName,
         Url: myUrl.href,
       })
     } catch (err) {
