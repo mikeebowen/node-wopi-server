5de2f28afdc6e9f4c3d7c16bfbe9b83a05d6dd74
Michael Bowen <mikee.bowen@gmail.com>
2021-05-26 11:28:16 -0700

    get wopi server name in html from .env

--- middleware/getFileNames.js
+++ middleware/getFileNames.js
@@ -7,12 +7,13 @@ module.exports = async (req, res, next) => {
   const folderPath = join(parse(process.cwd()).root, ...wopiStorageFolder)
   try {
     const files = await readdir(folderPath)
-    res.send(
-      files.map(f => {
+    res.send({
+      files: files.map(f => {
         const ext = extname(f)
         return { name: f, ext: ext.startsWith('.') ? ext.replace('.', '') : ext }
       }),
-    )
+      wopiServer: process.env.WOPI_SERVER,
+    })
   } catch (error) {
     return res.sendStatus(404)
   }
