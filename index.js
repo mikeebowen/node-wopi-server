bb3c966ef87973137d06a5bca548b5e5e16b08b8
Michael Bowen <mikee.bowen@gmail.com>
2022-03-09 14:24:44 -0800

    fix merge conflict

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const { getDiscoveryInfo, getFile, checkFileInfo, putFile, getRawBody, checkAccess, getFileNames, handleHeaders } = require('./middleware')
+const { createServer } = require('https')
+const { readFileSync } = require('fs')
 const port = process.env.PORT || 3000
+
+// const key = readFileSync('./certificates/wopi-key.pem')
+// const cert = readFileSync('./certificates/wopi-cert.pem')
+// const pfx = readFileSync(join(__dirname, 'certificates', 'dev-cert.pfx'))
+// const passphrase = 'p@ssw0rd'
+// const secureProtocol = 'TLSv1_2_method'
 
 app.use(getRawBody) // adds the raw binary of the post body to req.rawBody
 // app.get('*', getDiscoveryInfo)
@@ -20,10 +28,10 @@ app.get('/', (req, res, next) => {
   // res.sendFile(join(__dirname, 'SampleHostPage.html'))
   res.sendFile(join(__dirname, 'index.html'))
 })
-app.get('/case', (req, res, next) => {
-  // res.sendFile(join(__dirname, 'SampleHostPage.html'))
-  res.sendFile(join(__dirname, 'Load_iframe.html'))
-})
+
+// createServer({ pfx, passphrase, secureProtocol }, app).listen(port, () => {
+//   console.log(`server running on port ${port}`)
+// })
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
 })
