bed41aa222f1d1f9691062243463180a81efb4ba
Michael Bowen <mikee.bowen@gmail.com>
2022-04-29 12:28:53 -0700

    fix ExtendedLockLength test

--- middleware/getLock.js
+++ middleware/getLock.js
@@ -3,16 +3,13 @@ const { fileInfo } = require('../utils')
 
 module.exports = (req, res, next) => {
   const { file_id } = req.params
-  const lockValue = req.header('X-WOPI-Lock')
+  // const lockValue = req.header('X-WOPI-Lock')
 
   if (!Object.hasOwnProperty.call(fileInfo.lock, file_id)) {
     res.setHeader('X-WOPI-Lock', '')
     return res.sendStatus(200)
-  } else if (fileInfo.lock[file_id] === lockValue) {
-    res.setHeader('X-WOPI-Lock', lockValue)
-    return req.sendStatus(200)
   } else {
     res.setHeader('X-WOPI-Lock', fileInfo.lock[file_id] || '')
-    return res.sendStatus(409)
+    return res.sendStatus(200)
   }
 }
