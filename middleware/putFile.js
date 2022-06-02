87f4d0221473cfea3ecc42cafa43deb72ae8b45c
Michael Bowen <mikee.bowen@gmail.com>
2022-06-02 10:37:30 -0700

    linting

--- middleware/putFile.js
+++ middleware/putFile.js
@@ -1,32 +1,33 @@
-'use strict'
-const { join, parse } = require('path')
-const { stat, readdir } = require('fs/promises')
-const { fileInfo, updateFile } = require('../utils/')
+'use strict';
+const { join, parse } = require('path');
+const { stat, readdir } = require('fs/promises');
+const { fileInfo, updateFile } = require('../utils/');
 
 module.exports = async (req, res, next) => {
-  const { file_id } = req.params
+  const { file_id } = req.params;
 
   try {
-    const i = parseInt(file_id)
+    const i = parseInt(file_id);
 
-    const folderPath = join(process.cwd(), 'files')
-    const fileName = isNaN(i) ? req.params.file_id : (await readdir(folderPath)).sort()[i]
-    const filePath = join(folderPath, fileName)
+    const folderPath = join(process.cwd(), 'files');
+    const fileName = isNaN(i) ? req.params.file_id : (await readdir(folderPath)).sort()[i];
+    const filePath = join(folderPath, fileName);
 
-    const lockValue = req.header('X-WOPI-Lock')
-    const fileStats = await stat(filePath)
+    const lockValue = req.header('X-WOPI-Lock');
+    const fileStats = await stat(filePath);
 
     if ((!fileStats.size && !Object.hasOwnProperty.call(fileInfo.lock, file_id)) || (lockValue && fileInfo.lock[file_id] === lockValue)) {
-      fileInfo.lock[file_id] = lockValue
-      const time = await updateFile(filePath, req.rawBody, true)
-      res.setHeader('X-WOPI-ItemVersion', time)
-      return res.sendStatus(200)
+      fileInfo.lock[file_id] = lockValue;
+      const time = await updateFile(filePath, req.rawBody, true);
+      res.setHeader('X-WOPI-ItemVersion', time);
+      fileInfo.info.Version = time;
+      return res.sendStatus(200);
     } else {
-      res.setHeader('X-WOPI-Lock', fileInfo.lock[file_id] || '')
-      return res.sendStatus(409)
+      res.setHeader('X-WOPI-Lock', fileInfo.lock[file_id] || '');
+      return res.sendStatus(409);
     }
   } catch (error) {
-    res.setHeader('X-WOPI-Lock', fileInfo.lock[file_id] || '')
-    return res.sendStatus(409)
+    res.setHeader('X-WOPI-Lock', fileInfo.lock[file_id] || '');
+    return res.sendStatus(409);
   }
-}
+};
--- utils/updateFile.js
+++ utils/updateFile.js
@@ -1,24 +1,24 @@
-'use strict'
-const { createWriteStream, existsSync } = require('fs')
-const { stat, writeFile } = require('fs/promises')
-const fileInfo = require('./fileInfo')
+'use strict';
+const { createWriteStream, existsSync } = require('fs');
+const { stat, writeFile } = require('fs/promises');
+const fileInfo = require('./fileInfo');
 
 module.exports = async function updateFile(filePath, rawBody, updateVersion) {
   try {
     if (!existsSync(filePath)) {
-      await writeFile(filePath, new Uint8Array(Buffer.from('')))
+      await writeFile(filePath, new Uint8Array(Buffer.from('')));
     }
 
-    const wStream = createWriteStream(filePath)
-    wStream.write(rawBody)
-    const fileStats = await stat(filePath)
-    const time = fileStats.ctimeMs.toString()
+    const wStream = createWriteStream(filePath);
+    wStream.write(rawBody);
+    const fileStats = await stat(filePath);
+    const time = fileStats.mtimeMs.toString();
     if (updateVersion) {
-      fileInfo.info.Version = time
+      fileInfo.info.Version = time;
     }
-    return time
+    return time;
   } catch (err) {
-    console.error(err.message || err)
-    return 0
+    console.error(err.message || err);
+    return 0;
   }
-}
+};
