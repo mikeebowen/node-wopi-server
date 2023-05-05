a0dfd30b6ac96c0fc683a70122901487d33fa0c0
Michael Bowen <mikee.bowen@gmail.com>
2023-05-05 14:50:02 -0700

    use function declaration for asyncHandler instead of function expression with arrow function

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
-export const asyncHandler = (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-  Promise.resolve(fn(req, res, next))
-    .catch(next);
-};
+export function asyncHandler(fn: Function) {
+  return function(req: Request, res: Response, next: NextFunction) {
+    Promise.resolve(fn(req, res, next))
+      .catch(next);
+  };
+}
