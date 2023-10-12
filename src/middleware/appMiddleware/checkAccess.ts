8a74c4ba54a36d064d5791ee053bf53b6e215cc0
Michael Bowen <mikee.bowen@gmail.com>
2023-10-12 12:54:41 -0700

    store username and token in session storage

--- src/middleware/appMiddleware/checkAccess.ts
+++ src/middleware/appMiddleware/checkAccess.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 
 export function checkAccess(req: Request, res: Response, next: NextFunction) {
-  const { access_token: accessToken } = req.query;
-  const token = accessToken || req.header('authorization');
-  const ttl = req.query.access_token_ttl && typeof(req.query.access_token_ttl) === 'string' ? parseInt(req.query.access_token_ttl) : false;
+  const accessToken = String(req.query?.access_token);
+  const token = accessToken?.split('|')[0] ?? req.header('authorization');
+  const ttl = req.query.access_token_ttl && typeof (req.query.access_token_ttl) === 'string' ? parseInt(req.query.access_token_ttl) : false;
   const now = Date.now();
 
   if (
